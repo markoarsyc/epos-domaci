60272298c46ec732c9ad28062ec19ded348c7631
Marko Arsic <markoarsyc@gmail.com>
2023-11-18 02:20:04 +0100

    Uradjen script za drugu stranu

--- Script/script.js
+++ Script/script.js
@@ -11,3 +11,55 @@ function registracija() {
     window.location.href = 'registracija.html'
 }
 
+//Ljubmici
+
+function dona() {
+    let donaOpis = document.querySelector("#DONA");
+    donaOpis.style.display = "block";
+    let slika = document.querySelector("#DONA-SLIKA");
+    slika.style.display = "none";
+    //vrati Cicka
+    let cickoOpis = document.querySelector("#CICKO");
+    cickoOpis.style.display = "none";
+    let slikaCicka = document.querySelector("#CICKO-SLIKA");
+    slikaCicka.style.display = "block";
+    //vrati Milu
+    let milaOpis = document.querySelector("#MILA");
+    milaOpis.style.display = "none";
+    let slikaMila = document.querySelector("#MILA-SLIKA");
+    slikaMila.style.display = "block";
+}
+
+function cicko() {
+    let cickoOpis = document.querySelector("#CICKO");
+    cickoOpis.style.display = "block";
+    let slika = document.querySelector("#CICKO-SLIKA");
+    slika.style.display = "none";
+    //vrati Donu
+    let donaOpis = document.querySelector("#DONA");
+    donaOpis.style.display = "none";
+    let slikaDona = document.querySelector("#DONA-SLIKA");
+    slikaDona.style.display = "block";
+    //vrati Milu
+    let milaOpis = document.querySelector("#MILA");
+    milaOpis.style.display = "none";
+    let slikaMila = document.querySelector("#MILA-SLIKA");
+    slikaMila.style.display = "block";
+}
+
+function mila() {
+    let milaOpis = document.querySelector("#MILA");
+    milaOpis.style.display = "block";
+    let slika = document.querySelector("#MILA-SLIKA");
+    slika.style.display = "none";
+    //vrati Cicka
+    let cickoOpis = document.querySelector("#CICKO");
+    cickoOpis.style.display = "none";
+    let slikaCicka = document.querySelector("#CICKO-SLIKA");
+    slikaCicka.style.display = "block";
+    //vrati Donu
+    let donaOpis = document.querySelector("#DONA");
+    donaOpis.style.display = "none";
+    let slikaDona = document.querySelector("#DONA-SLIKA");
+    slikaDona.style.display = "block";
+}
