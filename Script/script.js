4cc5410caf6bc7a33c5264be8524c499cd8605b0
Marko Arsic <markoarsyc@gmail.com>
2023-11-18 01:45:19 +0100

    Ubacen JS za index.html

---
+++ Script/script.js
@@ -0,0 +1,13 @@
+//Za index.html
+
+//funkcije za redirekciju na druge stranice sajta
+function ljubimci() {
+    window.location.href = 'ljubimci.html'
+}
+function prodavnica() {
+    window.location.href = 'prodavnica.html'
+}
+function registracija() {
+    window.location.href = 'registracija.html'
+}
+
