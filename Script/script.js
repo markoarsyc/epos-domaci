d5de9b3a437436e6eb8cc5ee02c7d2d7572d406a
Marko Arsic <markoarsyc@gmail.com>
2023-11-19 00:53:48 +0100

    Gotova responzivnost za prve 3 strane

--- Script/script.js
+++ Script/script.js
@@ -13,60 +13,69 @@ function registracija() {
 
 //Ljubmici
 
+var sirinaProzora = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+console.log(sirinaProzora);
+
 function dona() {
-    let donaOpis = document.querySelector("#DONA");
-    donaOpis.style.display = "block";
-    let slika = document.querySelector("#DONA-SLIKA");
-    slika.style.display = "none";
-    //vrati Cicka
-    let cickoOpis = document.querySelector("#CICKO");
-    cickoOpis.style.display = "none";
-    let slikaCicka = document.querySelector("#CICKO-SLIKA");
-    slikaCicka.style.display = "block";
-    //vrati Milu
-    let milaOpis = document.querySelector("#MILA");
-    milaOpis.style.display = "none";
-    let slikaMila = document.querySelector("#MILA-SLIKA");
-    slikaMila.style.display = "block";
+    if (sirinaProzora >= 700) {
+        let donaOpis = document.querySelector("#DONA");
+        donaOpis.style.display = "block";
+        let slika = document.querySelector("#DONA-SLIKA");
+        slika.style.display = "none";
+        //vrati Cicka
+        let cickoOpis = document.querySelector("#CICKO");
+        cickoOpis.style.display = "none";
+        let slikaCicka = document.querySelector("#CICKO-SLIKA");
+        slikaCicka.style.display = "block";
+        //vrati Milu
+        let milaOpis = document.querySelector("#MILA");
+        milaOpis.style.display = "none";
+        let slikaMila = document.querySelector("#MILA-SLIKA");
+        slikaMila.style.display = "block";
+    } 
 }
 
 function cicko() {
-    let cickoOpis = document.querySelector("#CICKO");
-    cickoOpis.style.display = "block";
-    let slika = document.querySelector("#CICKO-SLIKA");
-    slika.style.display = "none";
-    //vrati Donu
-    let donaOpis = document.querySelector("#DONA");
-    donaOpis.style.display = "none";
-    let slikaDona = document.querySelector("#DONA-SLIKA");
-    slikaDona.style.display = "block";
-    //vrati Milu
-    let milaOpis = document.querySelector("#MILA");
-    milaOpis.style.display = "none";
-    let slikaMila = document.querySelector("#MILA-SLIKA");
-    slikaMila.style.display = "block";
+    if (sirinaProzora >= 700) {
+        let cickoOpis = document.querySelector("#CICKO");
+        cickoOpis.style.display = "block";
+        let slika = document.querySelector("#CICKO-SLIKA");
+        slika.style.display = "none";
+        //vrati Donu
+        let donaOpis = document.querySelector("#DONA");
+        donaOpis.style.display = "none";
+        let slikaDona = document.querySelector("#DONA-SLIKA");
+        slikaDona.style.display = "block";
+        //vrati Milu
+        let milaOpis = document.querySelector("#MILA");
+        milaOpis.style.display = "none";
+        let slikaMila = document.querySelector("#MILA-SLIKA");
+        slikaMila.style.display = "block";
+    }
 }
 
 function mila() {
-    let milaOpis = document.querySelector("#MILA");
-    milaOpis.style.display = "block";
-    let slika = document.querySelector("#MILA-SLIKA");
-    slika.style.display = "none";
-    //vrati Cicka
-    let cickoOpis = document.querySelector("#CICKO");
-    cickoOpis.style.display = "none";
-    let slikaCicka = document.querySelector("#CICKO-SLIKA");
-    slikaCicka.style.display = "block";
-    //vrati Donu
-    let donaOpis = document.querySelector("#DONA");
-    donaOpis.style.display = "none";
-    let slikaDona = document.querySelector("#DONA-SLIKA");
-    slikaDona.style.display = "block";
+    if (sirinaProzora >= 700) {
+        let milaOpis = document.querySelector("#MILA");
+        milaOpis.style.display = "block";
+        let slika = document.querySelector("#MILA-SLIKA");
+        slika.style.display = "none";
+        //vrati Cicka
+        let cickoOpis = document.querySelector("#CICKO");
+        cickoOpis.style.display = "none";
+        let slikaCicka = document.querySelector("#CICKO-SLIKA");
+        slikaCicka.style.display = "block";
+        //vrati Donu
+        let donaOpis = document.querySelector("#DONA");
+        donaOpis.style.display = "none";
+        let slikaDona = document.querySelector("#DONA-SLIKA");
+        slikaDona.style.display = "block";
+    }
 }
 
 //Prodavnica
 
 let opcije = document.querySelectorAll(".slika-i-opis p")
-for(let i = 0; i < opcije.length; i++) {
-    opcije[i].addEventListener('click', ()=>alert("Stranica u pripremi"));
+for (let i = 0; i < opcije.length; i++) {
+    opcije[i].addEventListener('click', () => alert("Stranica u pripremi"));
 }
